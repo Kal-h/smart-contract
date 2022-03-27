20d218f8e54ab3320fbe57c40a98f82fc5318f9a
MayJR <ht.hoi1995@gmail.com>
2022-03-27 17:03:06 +0700

    + Implement MultiSender contract.

---
+++ contracts/external/AbilityStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+/**
+ * @title AbilityStorage
+ * @dev This contract holds all the necessary state variables to support the upgrade functionality
+ */
+contract AbilityStorage {
+    // Version name of the current implementation
+    string internal _version;
+
+    // Address of the current implementation
+    address internal _implementation;
+
+    /**
+    * @dev Tells the version name of the current implementation
+    * @return string representing the name of the current version
+    */
+    function version() public view returns (string memory) {
+        return _version;
+    }
+
+    /**
+    * @dev Tells the address of the current implementation
+    * @return address of the current implementation
+    */
+    function implementation() public view returns (address) {
+        return _implementation;
+    }
+}
---
+++ deploy/01_deploy_multisender.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = await getChainId()
+
+    log("----------------------------------------------------")
+    const MultiSender = await deploy('MultiSender', {
+        from: deployer,
+        log: true
+    })
+    log(`You have deployed contract to ${MultiSender.address}`)
+}
+
+module.exports.tags = ['all', 'svg']
